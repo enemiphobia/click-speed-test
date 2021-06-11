f5b5b18010d3d2f04b16c0a0024f5711f2baa1d6
enemi44 <43891226+enemi44@users.noreply.github.com>
2021-06-11 17:45:48 -0400

    Update background.js

--- background.js
+++ background.js
@@ -1,1 +1,38 @@
-let cpsValue=document.getElementById("cpsButton"),clicks=0;document.getElementById("cpsButton").addEventListener("click",ClickMouse);function ClickMouse(){clicks++}setInterval(function(){cpsValue=document.getElementById("cpsButton"),cpsValue.innerHTML=clicks+" CPS",clicks=0},1e3);
+let cpsValue = document.getElementById("cpsButton");
+let recentCPS = document.getElementById("recent");
+let highscoreCPS = document.getElementById("highscore");
+highscore = localStorage.getItem("highScore");
+if (highscore != null || highscore != "null") {
+highscoreCPS.innerHTML = "Highscore: " + highscore;
+} else {
+    highscore = 0;
+}
+let clicks = 0;
+let recentClicks = 0;
+
+
+function ClickMouse() {
+    clicks++;
+    cpsValue.innerHTML = clicks + " CPS";
+
+    if (clicks <= 0) {
+        clicks = 0;
+    }
+    recentClicks = clicks;
+}
+
+document.getElementById("cpsButton").addEventListener("click", ClickMouse);
+
+setInterval(function() {
+    
+    cpsValue = document.getElementById("cpsButton");
+    cpsValue.innerHTML = clicks + " CPS";
+    recentCPS.innerHTML = "Your CPS: " + recentClicks;
+    if (clicks > parseInt(highscore) || highscore == null) {
+      highscore = clicks;
+      localStorage.setItem("highScore", highscore);
+      highscoreCPS.innerHTML = "Highscore: " + highscore;
+    }
+    clicks = 0;
+
+}, 1e3);
